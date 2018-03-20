fc7a711b9ca9b8cb28c50cbcb9093fd08f372c0a
jihye lim <tfj0531@gmail.com>
2018-03-20 22:27:30 +0900

    hflights df 변환

--- 180320.R
+++ 180320.R
@@ -1,3 +1,11 @@
 library(dplyr)
 install.packages("hflights")
 library(hflights)
+
+# hflights DB : 미국 휴스턴에서 출발하는 모든 비행기의 2011년 이착륙기록이 수록
+
+dim(hflights)
+
+hflights_df <- tbl_df(hflights)
+# tbl_df : dplyr 의 함수로 data frame 형태로 변환 
+hflights_df
